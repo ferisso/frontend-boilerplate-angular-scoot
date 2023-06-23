1e7e4f06df247e854cc2eda1200889fbc82691f3
fernando risso <f.risso6@hotmail.com>
2023-06-23 03:53:08 -0300

    handling todo's CRUD

--- app-frontend/src/app/app.component.ts
+++ app-frontend/src/app/app.component.ts
@@ -1,10 +1,154 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as dayjs from 'dayjs';
+
+interface ITodo {
+  id?: string
+  name: string;
+  description: string;
+  due?: Date;
+  dueFormat?: string;
+  priority: string;
+  status: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-frontend';
+
+export class AppComponent implements OnInit {
+  title = 'angular-todo';
+
+  constructor(private http: HttpClient) { }
+
+  todos: Array<ITodo> = [];
+  filteredTodos: Array<ITodo> = [];
+  search: string = "";
+  selectedPriority = "ALL"
+  priorityFilter = [
+    { text: 'All', color: '#10b981', value: 'ALL' },
+    { text: 'High', color: '#ef4444', value: 'HIGH' },
+    { text: 'Medium', color: '#eab308', value: 'MEDIUM' },
+    { text: 'Low', color: '#0284c7', value: 'LOW' },
+  ]
+  priority = [
+    { text: 'High', color: '#ef4444', value: 'HIGH' },
+    { text: 'Medium', color: '#eab308', value: 'MEDIUM' },
+    { text: 'Low', color: '#0284c7', value: 'LOW' },
+  ]
+  selectedStatus = false
+  status = [
+    { text: "To do", value: false },
+    { text: "Done", value: true },
+  ]
+  newTodo: boolean = false;
+  todoItem: ITodo = {
+    name: "",
+    description: "",
+    priority: "",
+    status: false,
+  }
+
+  async ngOnInit() {
+    await this.getTodos()
+  }
+
+  async getTodos() {
+    this.http.get('http://localhost:3001/todos')
+      .subscribe((res: any) => {
+        this.todos = res.map((item: any) => {
+          return {
+            ...item,
+            dueFormat: item.due && dayjs(item.due).format('MMMM/DD/YYYY')
+          }
+        })
+        this.filterPriorityAndStatus()
+      })
+  }
+
+  priorityColors = (data: string) => {
+    let color = {
+      'HIGH': '#ef4444',
+      'MEDIUM': '#eab308',
+      'LOW': '#0284c7'
+    }[data]
+    if (color) {
+      return color
+    }
+    return '#fff'
+  };
+
+  filterTodos() {
+    if (!!this.search) {
+      const filteredTodos = this.filteredTodos.filter((todo: any) => {
+        return todo.name.toUpperCase().includes(this.search.toUpperCase())
+      })
+      this.filteredTodos = filteredTodos
+      return;
+    }
+    this.filterPriorityAndStatus();
+  }
+
+  filterPriorityAndStatus() {
+    const filteredTodos = this.todos.filter((todo: any) => {
+      if (this.selectedPriority == "ALL") {
+        return todo.status == this.selectedStatus
+      }
+      return todo.priority == this.selectedPriority && todo.status == this.selectedStatus
+    })
+    this.filteredTodos = filteredTodos
+    return;
+  }
+
+  toggleCreateOrEditTodo(item?: ITodo) {
+    this.todoItem = {
+      name: "",
+      description: "",
+      priority: "",
+      status: false,
+    }
+    if (item) {
+      this.todoItem = { ...item }
+      delete this.todoItem.dueFormat
+    }
+    this.newTodo = true
+  }
+
+  createOrEditTodo() {
+    const headers = { 'content-type': 'application/json' }
+    const body = [this.todoItem]
+
+    this.http[this.todoItem.id ? 'put' : 'post']('http://localhost:3001/todos', body)
+      .subscribe((res) => {
+        this.getTodos()
+        this.newTodo = false
+      })
+  }
+
+  deleteTodo() {
+    this.http.delete(`http://localhost:3001/todos/${this.todoItem.id}`)
+      .subscribe(() => {
+        this.getTodos()
+        this.newTodo = false
+      })
+  }
+
+  changeStatus(todo: ITodo) {
+    this.newTodo = false
+    const body = [{
+      id: todo.id,
+      status: todo.status
+    }]
+    this.http.put('http://localhost:3001/todos', body)
+      .subscribe(() => {
+        this.getTodos()
+      })
+  }
+
+  cancel() {
+    this.newTodo = false
+  }
+
 }
--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -3,7 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -12,9 +26,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatSidenavModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
